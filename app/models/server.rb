e9d2dd54adabbee2403ee287b0fbc6c0e52ed493
Jonathan Calvert <jcalvert@vetstreet.com>
2011-09-19 23:04:26 -0400

    More bootstrapping

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,2 +1,20 @@
+java_import javax.management.remote.JMXConnector
+java_import javax.management.remote.JMXConnectorFactory
+java_import javax.management.remote.JMXServiceURL
+java_import javax.management.ObjectName
+
 class Server < ActiveRecord::Base
+  
+  @@connections = {} #TODO: this doesn't seem the best way, but it's what I'm doing atm
+  
+  def connection
+    if @@connections[name].nil?
+      url=JMXServiceURL.new uri
+      map = java.util.HashMap.new
+      creds = [username, password].to_java :String if !username.nil? #TODO: Encrypt pass?
+      jmxc = JMXConnectorFactory.connect url, {"jmx.remote.credentials" => creds}     
+      @@connections[name] = jmxc.get_mbean_server_connection
+    end
+  end
+  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'java'
+
+ROUTE_ATTRIBUTES = ["ExchangesCompleted", "ExchangesFailed", "ExchangesTotal",
+   "FirstExchangeCompletedTimestamp", "FirstExchangeFailureTimestamp", "InflightExchanges", 
+   "LastExchangeCompletedTimestamp", "LastExchangeFailureTimestamp", "LastProcessingTime",
+    "MaxProcessingTime", "MeanProcessingTime", "MinProcessingTime",
+    "RouteId", "RoutePolicyList", "State"]
 
 # Initialize the rails application
 CamelHerder::Application.initialize!
